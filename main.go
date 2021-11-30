ca358f1daf7a6f08725ef8b8865c3ce392c31dc0
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-11-30 14:20:02 +0900

    added conditional for heist on

--- main.go
+++ main.go
@@ -11,4 +11,13 @@ func main() {
 	isHeistOn := true
 	eludedGuards := rand.Intn(100)
 	fmt.Println("Test")
+
+	if eludedGuards >= 50 {
+		fmt.Println("Looks like you've managed to make it past the guards. Good job, but remember, this is the first step.")
+	} else {
+		isHeistOn = false
+		fmt.Println("Plan a better disguise next time?")
+	}
+
+	fmt.Printf("Is the heist on? %v", isHeistOn)
 }
